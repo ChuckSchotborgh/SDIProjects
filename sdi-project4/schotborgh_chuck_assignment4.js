6e25510fe4c46d520295afbf53276a1c7f99dc50
Chuck Schotborgh <chuckschotborgh@fullsail.edu>
2011-09-22 20:00:25 -0700

    Title Case Works

--- sdi-project4/schotborgh_chuck_assignment4.js
+++ sdi-project4/schotborgh_chuck_assignment4.js
@@ -53,6 +53,7 @@ verifyUrl("https://www.igt.com");
 	String Lib SDI 2011
 	Title-case a string (split into words, then uppercase the first letter of each word)
 */
+
 var capsCaseTitle = function (tCase) {
 		var wordVal = tCase.value;
 		var tCaseVal = '';
@@ -61,13 +62,10 @@ var capsCaseTitle = function (tCase) {
 	for(var i=0; i < wordVal.length; i++) {
 		tCaseVal += wordVal
 			[i].substring
-		(0,1).toUpperCase() + wordVal;
-			[i].substring
-		(1,wordVal[i].length) + ' ';
+		(0,1).toUpperCase() + wordVal[i].substring (1,wordVal[i].length) + ' ';
     }
     tCase.value = tCaseVal;// SampleBox can be tested in my Html
 }
-
 /*
 	Number Lib SDI 2011
 	Find the number of hours or days difference between two dates
@@ -81,26 +79,3 @@ var today = new Date()
 document.write("There are only "+daysLeft+" days left until Thanksgiving Day!");	
 console.log(daysLeft +" "+ "days left until Thanksgiving");
 
-/*
-    String 		
-
-    [x]Does a string follow a 123-456-7890 pattern like a phone number?
-    [x]Does a string follow an aaa@bbb.ccc pattern like an email address?
-    [x]Is the string a URL? (Does it start with http: or https:?)
-    [x]Title-case a string (split into words, then uppercase the first letter of each word)
-    * Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" ? "a/b/c".
-
-    Number
-    
-    [x]Format a number to use a specific number of decimal places, as for money: 2.1 ? 2.10
-    * Fuzzy-match a number: is the number above or below a number within a certain percent?
-    [x]Find the number of hours or days difference between two dates.
-    [x]Given a string version of a number such as "42", return the value as an actual Number, such as 42.
-
-    Array
-
-    [x]Find the smallest value in an array that is greater than a given number
-    * Find the total value of just the numbers in an array, even if some of the items are not numbers.
-    * Given an array of objects and the name of a key, return the array sorted by the value of that key in each of the objects: "a" + [{a:2},{a:3},{a:1}] ? [{a:1},{a:2},{a:3}].
-
- */
